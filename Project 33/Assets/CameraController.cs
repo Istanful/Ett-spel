133121efb39c397c3da90300116c49c114e1a0fc
Wildbook <book.wille@gmail.com>
2015-11-10 19:05:38 +0100

    Changed stuff :)

---
+++ Project 33/Assets/CameraController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+	public GameObject followObject;
+
+	void Update ()
+	{
+		transform.position = new Vector3 (followObject.transform.position.x + 65, followObject.transform.position.y + 24.5f, transform.position.z);
+	}
+}
