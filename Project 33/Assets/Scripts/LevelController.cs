c2b979714754f5ea9721d49ff543cc93fadb14e3
Wildbook <book.wille@gmail.com>
2015-11-12 15:37:03 +0100

    Början till att fixa bakgrunder.

---
+++ Project 33/Assets/Scripts/LevelController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelController : MonoBehaviour {
+
+    public GameObject background;
+    public GameObject scenery;
+    public GameObject ground;
+
+    public float backgroundMoveSpeed = 1;
+    public float sceneryMoveSpeed = 2;
+    public float groundMoveSpeed = 3;
+
+    Vector3 startPosition;
+    Vector3 endPosition;
+    float speed = 10.0f;
+
+    
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+
+	void Update () {
+        transform.position = Vector3.Lerp(startPosition, endPosition, speed * Time.deltaTime);
+    }
+}
