27af431f7c5f63971246a16dd7324a0a5ce437b4
Wildbook <book.wille@gmail.com>
2015-12-31 00:54:45 +0100

    New build, running sounds + more enemies

---
+++ Project 33/Assets/SoundOnSprite.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class SoundOnSprite : MonoBehaviour
+{
+    [SerializeField]
+    public TriggerSprite[] triggeringSprites;
+    
+    SpriteRenderer spriteRenderer;
+    Sprite oldSprite;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        foreach (TriggerSprite ts in triggeringSprites)
+        {
+            ts.audioSource = gameObject.AddComponent<AudioSource>();
+            ts.audioSource.clip = ts.audioClip;
+        }
+    }
+    
+    void Update()
+    {
+        if (spriteRenderer.sprite != oldSprite)
+        {
+            foreach (TriggerSprite ts in triggeringSprites)
+                if (ts.triggerSprite == spriteRenderer.sprite)
+                    ts.audioSource.PlayOneShot(ts.audioClip);
+            
+            oldSprite = spriteRenderer.sprite;
+        }
+    }
+
+    [System.Serializable]
+    public class TriggerSprite
+    {
+        public Sprite triggerSprite;
+        public AudioClip audioClip;
+
+        [HideInInspector]
+        public AudioSource audioSource;
+
+        [Range(0, 1)]
+        public float volume = 1;
+    }
+}
